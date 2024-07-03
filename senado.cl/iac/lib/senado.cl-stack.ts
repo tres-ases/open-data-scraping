dc4a1d3a403a7bb97b8c4bbbca24a2ae71a15ab4
jvillane <jvillane@gmail.com>
2024-07-03 11:14:49 -0400

    ADD: step function to extract dieta-ano-mes

--- senado.cl/iac/lib/senado.cl-stack.ts
+++ senado.cl/iac/lib/senado.cl-stack.ts
@@ -1,9 +1,12 @@
 import {
+  Duration,
   Stack,
   StackProps,
   aws_lambda_nodejs as nodejs,
 } from 'aws-cdk-lib';
 import {Architecture, Code, LayerVersion, Runtime} from 'aws-cdk-lib/aws-lambda';
+import {DefinitionBody, StateMachine, Succeed} from 'aws-cdk-lib/aws-stepfunctions';
+import {LambdaInvoke}  from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import {Construct} from 'constructs';
 
 export class SenadoClStack extends Stack {
@@ -39,5 +42,31 @@ export class SenadoClStack extends Stack {
         layers: [commonsLy, scraperLy]
       }
     );
+
+    const dietaAnoMesFn = new nodejs.NodejsFunction(this, 'simple-function', {
+        code: Code.fromAsset('../code/packages/Dieta-AnoMes/dist'),
+        handler: 'dieta-anomes.handler',
+        runtime: Runtime.NODEJS_20_X,
+        layers: [commonsLy, scraperLy]
+      }
+    );
+
+    const dietaAnoMesJob = new LambdaInvoke(
+      this,
+      "dieta-ano-mes-job", {
+        lambdaFunction: dietaAnoMesFn,
+      }
+    );
+
+    const stateMachineDefinition = dietaAnoMesJob
+      .next(new Succeed(this, "dieta-ano-mes-succeed"));
+
+    const stateMachine = new StateMachine(this, "state-machine", {
+      definitionBody: DefinitionBody.fromChainable(
+        stateMachineDefinition
+      ),
+      timeout: Duration.minutes(5),
+      stateMachineName: "ProcessAndReportJob",
+    });
   }
 }
