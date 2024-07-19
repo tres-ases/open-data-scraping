9426744709523d419a74b3b66cd438fdf84dbb3e
jvillane <jvillane@gmail.com>
2024-07-19 12:08:20 -0400

    ADD: votaciones: legislaturas & sesiones code & iac (2)

--- senado.cl/packages/Votaciones/src/votaciones.ts
+++ senado.cl/packages/Votaciones/src/votaciones.ts
@@ -1,8 +1,8 @@
 import {Handler} from "aws-lambda";
 import {getSaveLegislaturaSesiones, getSaveLegislaturaSimpleList} from "./votaciones-legislatura.service";
 
-export const getSaveLegislaturasHandler: Handler = async () => {
-  return await getSaveLegislaturaSimpleList();
+export const getSaveLegislaturasHandler: Handler<{cantidad: number}> = async ({cantidad}) => {
+  return await getSaveLegislaturaSimpleList(cantidad);
 };
 
 export const getSaveLegislaturasSesionesHandler: Handler<{legisId: number}> = async ({legisId}) => {
