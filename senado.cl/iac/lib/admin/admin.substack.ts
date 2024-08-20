0d449fc890497fea4bc8c7e9e5371655d3b74e15
jvillane <jvillane@gmail.com>
2024-08-20 16:41:57 -0400

    FIX: cloudfront origins/behaviors/permissions (14)

--- senado.cl/iac/lib/admin/admin.substack.ts
+++ senado.cl/iac/lib/admin/admin.substack.ts
@@ -88,7 +88,11 @@ export default class AdminSubstack extends NestedStack {
 
   getLogicalId(element: CfnElement): string {
     if (element.node.id.includes('NestedStackResource')) {
-      return /([a-zA-Z0-9]+)\.NestedStackResource/.exec(element.node.id)![1] // will be the exact id of the stack
+      try {
+        return /([a-zA-Z0-9]+)\.NestedStackResource/.exec(element.node.id)![1] // will be the exact id of the stack
+      } catch (e) {
+
+      }
     }
     return super.getLogicalId(element)
   }
