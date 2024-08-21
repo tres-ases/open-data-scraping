ee98663f2a69ae066d915a7a4439f0975ea55ca8
jvillane <jvillane@gmail.com>
2024-08-21 10:22:52 -0400

    FIX: cloudfront origins/behaviors/permissions (27)

--- senado.cl/iac/lib/admin/admin.substack.ts
+++ senado.cl/iac/lib/admin/admin.substack.ts
@@ -100,25 +100,38 @@ export default class AdminSubstack extends NestedStack {
     const distribution = new Distribution(scope, 'cloudfront-distribution', {
       //domainNames: [subdomain],
       defaultBehavior: {
-        origin: new S3Origin(hostingBucket, {
-          originAccessIdentity: oai,
-          originPath: '/',
-        }),
-        cachePolicy: CachePolicy.CACHING_OPTIMIZED_FOR_UNCOMPRESSED_OBJECTS,
+        origin: new HttpOrigin(`${api.restApiId}.execute-api.${api.env.region}.amazonaws.com`),
+        allowedMethods: AllowedMethods.ALLOW_ALL,
+        cachePolicy: CachePolicy.CACHING_DISABLED,
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-      },
-      additionalBehaviors: {
-        'api/*': {
-          origin: new HttpOrigin(`${api.restApiId}.execute-api.${api.env.region}.amazonaws.com`),
-          allowedMethods: AllowedMethods.ALLOW_ALL,
-          cachePolicy: CachePolicy.CACHING_DISABLED,
-          viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        },
       },
       defaultRootObject: 'index.html',
       priceClass: PriceClass.PRICE_CLASS_ALL,
       //certificate
     });
+
+    //const distribution = new Distribution(scope, 'cloudfront-distribution', {
+    //  //domainNames: [subdomain],
+    //  defaultBehavior: {
+    //    origin: new S3Origin(hostingBucket, {
+    //      originAccessIdentity: oai,
+    //      originPath: '/',
+    //    }),
+    //    cachePolicy: CachePolicy.CACHING_OPTIMIZED_FOR_UNCOMPRESSED_OBJECTS,
+    //    viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    //  },
+    //  additionalBehaviors: {
+    //    'api/*': {
+    //      origin: new HttpOrigin(`${api.restApiId}.execute-api.${api.env.region}.amazonaws.com`),
+    //      allowedMethods: AllowedMethods.ALLOW_ALL,
+    //      cachePolicy: CachePolicy.CACHING_DISABLED,
+    //      viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    //    },
+    //  },
+    //  defaultRootObject: 'index.html',
+    //  priceClass: PriceClass.PRICE_CLASS_ALL,
+    //  //certificate
+    //});
 
     //new ARecord(this, `${prefix}-alias-record`, {
     //  zone,
