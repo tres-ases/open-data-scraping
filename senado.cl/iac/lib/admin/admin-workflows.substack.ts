6419bbb4730811e3bbdfdbcc2f113db0ca41ac6e
jvillane <jvillane@gmail.com>
2024-09-03 15:30:31 -0400

    ADD: Sesiones

--- senado.cl/iac/lib/admin/admin-workflows.substack.ts
+++ senado.cl/iac/lib/admin/admin-workflows.substack.ts
@@ -44,7 +44,10 @@ export default class AdminWorkflowsSubstack extends NestedStack {
 
   getLogicalId(element: CfnElement): string {
     if (element.node.id.includes('NestedStackResource')) {
-      return /([a-zA-Z0-9]+)\.NestedStackResource/.exec(element.node.id)![1]
+      try {
+        return /([a-zA-Z0-9]+)\.NestedStackResource/.exec(element.node.id)![1]
+      } catch (e) {
+      }
     }
     return super.getLogicalId(element)
   }
