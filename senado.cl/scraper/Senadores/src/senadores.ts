0ad2723d1fc690bc76347a21132a65269c728f91
jvillane <jvillane@gmail.com>
2024-10-09 22:57:53 -0300

    WIP: distilled data

--- senado.cl/scraper/Senadores/src/senadores.ts
+++ senado.cl/scraper/Senadores/src/senadores.ts
@@ -1,16 +1,21 @@
+import {Logger} from "@aws-lambda-powertools/logger";
 import {Handler} from "aws-lambda";
 import { SQSHandler } from 'aws-lambda';
 import {detectNewSlugs, getSaveSenador} from "./senadores.service";
+
+const logger = new Logger();
 
 interface GetSaveQueueHandlerProps {
   slug: string
 }
 
 export const getSaveHandler: Handler<GetSaveQueueHandlerProps> = async ({slug}) => {
+  logger.info('Ejecutando getSaveHandler - slug', slug);
   return await getSaveSenador(slug);
 }
 
 export const getSaveQueueHandler: SQSHandler = async ({Records}) => {
+  logger.info('Ejecutando getSaveQueueHandler - Records', JSON.stringify(Records));
   await Promise.all(
     Records.map(async (record) => getSaveSenador(record.body))
   )
@@ -21,5 +26,6 @@ interface DetectNewSlugsHandlerProps {
 }
 
 export const detectNewSlugsHandler: Handler<DetectNewSlugsHandlerProps> = async ({legId}) => {
+  logger.info('Ejecutando detectNewSlugsHandler - legId', legId);
   return await detectNewSlugs(legId);
 }
