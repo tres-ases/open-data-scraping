1712d4f14b042f430435c139cc4d16bf4e8e9f17
jvillane <jvillane@gmail.com>
2024-11-18 20:35:11 -0300

    ADD: votaciones pre table

--- senado.cl/global/src/config/votaciones.config.ts
+++ senado.cl/global/src/config/votaciones.config.ts
@@ -1,4 +1,4 @@
 export const VotacionesBucketKey = {
-  table: (sesId: string | number) => `table/sesion_votacion/sesId=${sesId}/data.json`,
-  tableDetalle: (votId: string | number) => `table/sesion_votacion_detalle/votId=${votId}/data.json`,
+  table: (sesId: string | number) => `pre_table/sesion_votacion/sesId=${sesId}/data.json`,
+  tableDetalle: (votId: string | number) => `pre_table/sesion_votacion_detalle/votId=${votId}/data.json`,
 }
