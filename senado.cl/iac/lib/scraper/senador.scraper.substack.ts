4fcb97ffd123e62ee68d11110f4ef73b22aaa656
jvillane <jvillane@gmail.com>
2024-12-12 19:37:52 -0300

    REFACTOR: iac

--- senado.cl/iac/lib/scraper/senador.scraper.substack.ts
+++ senado.cl/iac/lib/scraper/senador.scraper.substack.ts
@@ -19,19 +19,11 @@ export default class SenadorScraperSubStack extends NestedStack {
   constructor(scope: Construct, id: string, {bucket, connection, senadorQueue}: Props) {
     super(scope, id);
 
-    const sfRole = new Role(this, `${id}-role`, {
+    const sfRole = new Role(this, `${id}-smRole`, {
+      roleName: `${id}-smRole`,
       assumedBy: new ServicePrincipal('states.amazonaws.com'),
     });
-
     bucket.grantReadWrite(sfRole);
-    sfRole.addToPolicy(
-      new PolicyStatement({
-        effect: Effect.ALLOW,
-        actions: ['sts:AssumeRole'],
-        principals: [new ServicePrincipal('pipes.amazonaws.com')],
-        resources: [connection.connectionArn]
-      })
-    );
 
     let definition = fs.readFileSync('./lib/scraper/asl/senador.asl.json', 'utf8');
 
@@ -57,27 +49,19 @@ export default class SenadorScraperSubStack extends NestedStack {
       })
     );
 
-    const pipeRole = new Role(this, `${id}-pipe-role`, {
-      assumedBy: new ServicePrincipal('pipes.amazonaws.com'),
-      inlinePolicies: {
-        AppPolicy: new PolicyDocument({
-          statements: [
-            new PolicyStatement({
-              effect: Effect.ALLOW,
-              actions: ['states:StartExecution'],
-              resources: [sm.attrArn],
-            }),
-            new PolicyStatement({
-              effect: Effect.ALLOW,
-              actions: ['sqs:ReceiveMessage', 'sqs:DeleteMessage', 'sqs:GetQueueAttributes'],
-              resources: [senadorQueue.queueArn],
-            }),
-          ],
-        }),
-      },
+    const pipeRole = new Role(this, `${id}-pipeRole`, {
+      roleName: `${id}-pipeRole`,
+      assumedBy: new ServicePrincipal('pipes.amazonaws.com')
     });
+    senadorQueue.grantConsumeMessages(pipeRole);
+    pipeRole.addToPolicy(new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["states:StartExecution"],
+      resources: [sm.attrArn]
+    }));
 
     new CfnPipe(this, `${id}-pipe`, {
+      name: `${id}-pipe`,
       roleArn: pipeRole.roleArn,
       source: senadorQueue.queueArn,
       target: sm.attrArn,
