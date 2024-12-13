4f04ed8eb6fce51c2a61e65a6748473c6b2c40e8
jvillane <jvillane@gmail.com>
2024-12-12 22:07:26 -0300

    REFACTOR: iac

--- senado.cl/iac/lib/scraper/senador.scraper.substack.ts
+++ senado.cl/iac/lib/scraper/senador.scraper.substack.ts
@@ -1,6 +1,6 @@
 import {CfnOutput, NestedStack, NestedStackProps, RemovalPolicy} from "aws-cdk-lib";
 import {Connection} from "aws-cdk-lib/aws-events";
-import {Effect, PolicyStatement, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {Effect, Policy, PolicyStatement, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
 import {Bucket} from "aws-cdk-lib/aws-s3";
 import {CfnStateMachine, StateMachineType} from "aws-cdk-lib/aws-stepfunctions";
 import {Construct} from "constructs";
@@ -26,25 +26,54 @@ export default class SenadorScraperSubStack extends NestedStack {
       retention: RetentionDays.THREE_MONTHS
     });
 
-    const sfRole = new Role(this, `${id}-smRole`, {
+    const smRole = new Role(this, `${id}-smRole`, {
       assumedBy: new ServicePrincipal('states.amazonaws.com'),
     });
-    bucket.grantReadWrite(sfRole);
-    sfRole.addToPolicy(
-      new PolicyStatement({
-        effect: Effect.ALLOW,
-        resources: ['*'],
-        actions: [
-          'logs:*'
-        ],
-      })
-    );
+    const smRolePolicy = new Policy(this, 'smPolicy', {
+      policyName: `${id}-smPolicy`,
+      statements: [
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: [
+            's3:Abort*',
+            's3:DeleteObject*',
+            's3:GetBucket*',
+            's3:GetObject*',
+            's3:List*',
+            's3:PutObject',
+            's3:PutObjectLegalHold',
+            's3:PutObjectRetention',
+            's3:PutObjectTagging',
+            's3:PutObjectVersionTagging'
+          ],
+          resources: [
+            bucket.bucketArn,
+            `${bucket.bucketArn}/*`
+          ]
+        }),
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: [
+            'logs:CreateLogDelivery',
+            'logs:GetLogDelivery',
+            'logs:UpdateLogDelivery',
+            'logs:DeleteLogDelivery',
+            'logs:ListLogDeliveries',
+            'logs:PutResourcePolicy',
+            'logs:DescribeResourcePolicies',
+            'logs:DescribeLogGroups'
+          ],
+          resources: [logGroup.logGroupArn],
+        })
+      ]
+    });
+    smRole.attachInlinePolicy(smRolePolicy);
 
     let definition = fs.readFileSync('./lib/scraper/asl/senador.asl.json', 'utf8');
 
     const sm = new CfnStateMachine(this, `${id}-sm`, {
       stateMachineName: `${id}-sm`,
-      roleArn: sfRole.roleArn,
+      roleArn: smRole.roleArn,
       definitionString: definition,
       definitionSubstitutions: {
         events_connection_arn: connection.connectionArn,
@@ -64,14 +93,16 @@ export default class SenadorScraperSubStack extends NestedStack {
         level: 'ALL',
       }
     });
-    sfRole.addToPolicy(
-      new PolicyStatement({
-        sid: 'InvokeHttpEndpoint',
-        effect: Effect.ALLOW,
-        actions: ["states:InvokeHTTPEndpoint"],
-        resources: [sm.attrArn]
-      })
-    );
+    smRole.attachInlinePolicy(new Policy(this, 'smPolicyInvokeHTTPEndpoint', {
+      statements: [
+        new PolicyStatement({
+          sid: 'InvokeHttpEndpoint',
+          effect: Effect.ALLOW,
+          actions: ["states:InvokeHTTPEndpoint"],
+          resources: [sm.attrArn]
+        })
+      ]
+    }));
 
     const pipeRole = new Role(this, `${id}-pipeRole`, {
       roleName: `${id}-pipeRole`,
