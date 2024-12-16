2ee2438f48d585a8bfb517ec465021192a45d4c3
jvillane <jvillane@gmail.com>
2024-12-16 09:24:20 -0300

    REFACTOR: iac

--- senado.cl/iac/bin/senado.cl.ts
+++ senado.cl/iac/bin/senado.cl.ts
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
+import 'dotenv/config';
 import * as cdk from 'aws-cdk-lib';
 import MainStack from "../lib/main.stack";
 
 const app = new cdk.App();
 console.log('process.env', JSON.stringify(process.env));
-new MainStack(app, 'SenadoCl', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-});
+
+if(process.env.CDK_DEFAULT_ACCOUNT === undefined) {
+  console.error('process.env.CDK_DEFAULT_ACCOUNT no existe!');
+}
+
+else {
+  new MainStack(app, 'SenadoCl', {
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  });
+}
