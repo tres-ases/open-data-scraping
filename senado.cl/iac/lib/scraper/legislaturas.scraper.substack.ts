7be8be6b6b2d805b077de1427d24001df83a8a37
jvillane <jvillane@gmail.com>
2024-12-18 22:42:50 -0300

    REFACTOR: saving legislaturas on DynDb

--- senado.cl/iac/lib/scraper/legislaturas.scraper.substack.ts
+++ senado.cl/iac/lib/scraper/legislaturas.scraper.substack.ts
@@ -9,14 +9,13 @@ import {LogGroup, RetentionDays} from "aws-cdk-lib/aws-logs";
 import {Table} from "aws-cdk-lib/aws-dynamodb";
 
 interface Props extends NestedStackProps {
-  bucket: Bucket
   connection: Connection
   legislaturasTable: Table
 }
 
 export default class LegislaturasScraperSubStack extends NestedStack {
 
-  constructor(scope: Construct, id: string, {bucket, connection, legislaturasTable}: Props) {
+  constructor(scope: Construct, id: string, {connection, legislaturasTable}: Props) {
     super(scope, id);
 
     const logGroup = new LogGroup(this, `${id}-smLogs`, {
@@ -31,25 +30,6 @@ export default class LegislaturasScraperSubStack extends NestedStack {
     const smRolePolicy = new Policy(this, 'smPolicy', {
       policyName: `${id}-smPolicy`,
       statements: [
-        new PolicyStatement({
-          effect: Effect.ALLOW,
-          actions: [
-            's3:Abort*',
-            's3:DeleteObject*',
-            's3:GetBucket*',
-            's3:GetObject*',
-            's3:List*',
-            's3:PutObject',
-            's3:PutObjectLegalHold',
-            's3:PutObjectRetention',
-            's3:PutObjectTagging',
-            's3:PutObjectVersionTagging'
-          ],
-          resources: [
-            bucket.bucketArn,
-            `${bucket.bucketArn}/*`
-          ]
-        }),
         new PolicyStatement({
           sid: 'RetrieveConnectionCredentials',
           effect: Effect.ALLOW,
@@ -98,7 +78,7 @@ export default class LegislaturasScraperSubStack extends NestedStack {
       definitionString: definition,
       definitionSubstitutions: {
         events_connection_arn: connection.connectionArn,
-        bucket_name: bucket.bucketName,
+        legislaturas_table_name: legislaturasTable.tableName,
       },
       stateMachineName: `${id}-sm`,
       stateMachineType: StateMachineType.EXPRESS,
@@ -128,8 +108,8 @@ export default class LegislaturasScraperSubStack extends NestedStack {
     new CfnOutput(this, '${events_connection_arn}', {
       value: connection.connectionArn,
     });
-    new CfnOutput(this, '${bucket_name}', {
-      value: bucket.bucketName,
+    new CfnOutput(this, '${legislaturas_table_name}', {
+      value: legislaturasTable.tableName,
     });
   }
 }
