ca2caed3b8fe5c06a9213d31c2cc6b48877ae81a
jvillane <jvillane@gmail.com>
2024-12-29 13:46:45 -0300

    WIP: generate tables

--- senado.cl/iac/lib/main.stack.ts
+++ senado.cl/iac/lib/main.stack.ts
@@ -18,7 +18,7 @@ export default class MainStack extends Stack {
     });
 
     const parlamentarioImagenQueue = new Queue(this, `${id}-parl-img-queue`, {
-      queueName: `${id}-parl-img-queue`,
+      queueName: `${id}-parl-img-queue.fifo`,
       visibilityTimeout: Duration.minutes(15),
       contentBasedDeduplication: true,
     });
