bb03eb545270fbda76d7b9c9d68029a39ff1a0e2
jvillane <jvillane@gmail.com>
2024-12-28 21:51:41 -0300

    WIP: generate tables

---
+++ senado.cl/iac/lib/tables/query/table-proyectos.sql
@@ -0,0 +1,23 @@
+CREATE TABLE "senado_cl"."table-proyectos"
+WITH
+(
+  format='JSON',
+  external_location='s3://open-data-cl-bucket/tables/table-proyectos'
+) AS
+
+SELECT
+    CAST(boletin AS INT) AS boletin_id,
+    info.descripcion.boletin AS boletin,
+    info.descripcion.camaraOrigen AS camara_origen,
+    info.descripcion.diarioOficial AS diario_oficial,
+    info.descripcion.estado,
+    info.descripcion.etapa,
+    info.descripcion.subEtapa AS subetapa,
+    info.descripcion.fechaIngreso AS fecha_ingreso,
+    info.descripcion.iniciativa,
+    info.descripcion.leyNumero AS ley_numero,
+    info.descripcion.linkMensajeMocion AS link_mensaje_mocion,
+    info.descripcion.urgenciaActual AS urgencia_actual,
+    info.descripcion.titulo,
+    fechaModificacion AS fecha_modificacion
+FROM "AthenaDynamoDb"."default"."senado-raw-proyectos"
---
+++ senado.cl/iac/lib/tables/table-query-list.tables.substack.ts
@@ -0,0 +1,109 @@
+import {CfnElement, NestedStack, NestedStackProps, RemovalPolicy} from "aws-cdk-lib";
+import {CfnStateMachine, StateMachineType} from "aws-cdk-lib/aws-stepfunctions";
+import {Construct} from "constructs";
+import * as fs from "fs";
+import {LogGroup, RetentionDays} from "aws-cdk-lib/aws-logs";
+import {Effect, Policy, PolicyStatement, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {Bucket} from "aws-cdk-lib/aws-s3";
+import {Table} from "aws-cdk-lib/aws-dynamodb";
+
+interface Props extends NestedStackProps {
+  recreateTableStateMachine: CfnStateMachine
+}
+
+export default class TableQueryListSubStack extends NestedStack {
+  constructor(scope: Construct, id: string, {
+    recreateTableStateMachine
+  }: Props) {
+    super(scope, id);
+
+    const logGroup = new LogGroup(this, `${id}-smLogs`, {
+      logGroupName: `/aws/vendedlogs/states/${id}-sm`,
+      removalPolicy: RemovalPolicy.DESTROY,
+      retention: RetentionDays.THREE_MONTHS
+    });
+
+    const smRole = new Role(this, `${id}-smRole`, {
+      assumedBy: new ServicePrincipal('states.amazonaws.com'),
+    });
+    const smRolePolicy = new Policy(this, `${id}-smPolicy`, {
+      policyName: `${id}-smPolicy`,
+      statements: [
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: [
+            'logs:CreateLogDelivery',
+            'logs:CreateLogStream',
+            'logs:GetLogDelivery',
+            'logs:UpdateLogDelivery',
+            'logs:DeleteLogDelivery',
+            'logs:ListLogDeliveries',
+            'logs:PutLogEvents',
+            'logs:PutResourcePolicy',
+            'logs:DescribeResourcePolicies',
+            'logs:DescribeLogGroups'
+          ],
+          resources: ['*'],
+        }),
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: [
+            'xray:PutTraceSegments',
+            'xray:PutTelemetryRecords',
+            'xray:GetSamplingRules',
+            'xray:GetSamplingTargets'
+          ],
+          resources: ['*'],
+        }),
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: ['states:StartSyncExecution'],
+          resources: [recreateTableStateMachine.attrArn]
+        }),
+      ]
+    });
+    smRole.attachInlinePolicy(smRolePolicy);
+
+    const definition = fs.readFileSync('./lib/tables/asl/table-query-list.asl.json', 'utf8');
+
+    const tableQueries = [
+      'table-proyectos'
+    ].map(table => ({
+      table, query: fs.readFileSync(`./lib/tables/query/${table}.sql`, 'utf8')
+    }))
+
+    new CfnStateMachine(this, `${id}-sm`, {
+      roleArn: smRole.roleArn,
+      definitionString: definition,
+      stateMachineName: `${id}-sm`,
+      definitionSubstitutions: {
+        table_queries: JSON.stringify(tableQueries),
+        recreate_table_state_machine: recreateTableStateMachine.attrArn
+      },
+      stateMachineType: StateMachineType.STANDARD,
+      tracingConfiguration: {
+        enabled: true
+      },
+      loggingConfiguration: {
+        destinations: [{
+          cloudWatchLogsLogGroup: {
+            logGroupArn: logGroup.logGroupArn
+          }
+        }],
+        includeExecutionData: true,
+        level: 'ALL',
+      }
+    });
+  }
+
+  getLogicalId(element: CfnElement): string {
+    if (element.node.id.includes('NestedStackResource')) {
+      try {
+        return /([a-zA-Z0-9]+)\.NestedStackResource/.exec(element.node.id)![1] // will be the exact id of the stack
+      } catch (e) {
+
+      }
+    }
+    return super.getLogicalId(element)
+  }
+}
