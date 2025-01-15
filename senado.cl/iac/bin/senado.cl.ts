5745f28da58989d3b0449dcd53e773a7928619cd
jvillane <jvillane@gmail.com>
2025-01-14 23:26:06 -0300

    Enhance logging in senado.cl.ts: added console log for environment variables to aid in debugging and ensure correct configuration during AWS CDK deployment.

--- senado.cl/iac/bin/senado.cl.ts
+++ senado.cl/iac/bin/senado.cl.ts
@@ -5,6 +5,8 @@ import MainStack from "../lib/main.stack";
 
 const app = new cdk.App();
 
+console.log('process.env',JSON.stringify(process.env));
+
 new MainStack(app, 'SenCl', {
   env: {account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION},
 });
